d3b725f97b75f0de31ab3c53192165eee271435d
Jatin wadhwa <jatinw21@gmail.com>
2016-12-03 23:05:57 +1100

    need to declare sum = 0 before starting addition

--- algorithms/warmup/simpleArraySum.py
+++ algorithms/warmup/simpleArraySum.py
@@ -14,6 +14,8 @@ splitLineList = numArrayLine.split(' ')
 # print(splitLineList)
 
 # going through list and calculating sum
+
+# NOTE: need to first declare sum = 0 before starting adding
 sum = 0
 for num in splitLineList:
     sum += int(num)
