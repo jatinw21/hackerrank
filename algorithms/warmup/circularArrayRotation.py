a0d92b5ae3056725a3e381f5ea3aa5a5dab55260
Jatin wadhwa <jatinw21@gmail.com>
2016-12-04 12:12:43 +1100

    major performance improvement. No need to copy list. Although learn to copy

--- algorithms/warmup/circularArrayRotation.py
+++ algorithms/warmup/circularArrayRotation.py
@@ -5,14 +5,17 @@ numInts, numRotations, numQueries = [int(x) for x in input().split(' ')]
 # print(numInts, numRotations, numQueries)
 
 intList = [int(x) for x in input().split(' ')]
+
 for j in range(numQueries):
     query = int(input())
-    # http://stackoverflow.com/questions/2612802/how-to-clone-or-copy-a-list
-    # NOTE: for copying such that changing copy doesn't affect original, slicing is fastes
-    # NOTE: But if original list contains list, dictionaries or class instances, changing them would change in original,
-    # NOTE: so for that we need copy.deepcopy(old_list)
-    listCopy = intList[:]
-    for i in range(numRotations):
-        removed = listCopy.pop()
-        listCopy.insert(0, removed)
-    print(listCopy[query])
+    index = (query - numRotations) % numInts
+    print(intList[index])
+
+
+
+
+# SIDENOTE FOR THE OTHER LONGER METHOD - not reqd here but good to know
+# http://stackoverflow.com/questions/2612802/how-to-clone-or-copy-a-list
+# NOTE: for copying such that changing copy doesn't affect original, slicing is fastes
+# NOTE: But if original list contains list, dictionaries or class instances, changing them would change in original,
+# NOTE: so for that we need copy.deepcopy(old_list)
