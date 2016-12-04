95386c103fc0810014666096899365beef6af872
Jatin wadhwa <jatinw21@gmail.com>
2016-12-04 22:01:47 +1100

    splitting string by characters into list, converting to ascii, num of items in list

---
+++ algorithms/implementation/designerPdfViewer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+heightList = [int(x) for x in input().split(' ')]
+
+characs = input()
+
+# NOTE: Use this method to split on every n characs. Replace 1 by 'n'
+[characs[i:i+1] for i in range(0, len(characs), 1)]
+
+# NOTE: num of elements in a list
+length = len(characs)
+
+maxHeight = 0
+
+for i in characs:
+    # DEBUG:
+    # print(ord(i)-ord('a'))
+
+    # NOTE: use ord() for ASCII value
+    if heightList[ord(i)-ord('a')] > maxHeight:
+        maxHeight = heightList[ord(i)-ord('a')]
+
+print(length*maxHeight)
