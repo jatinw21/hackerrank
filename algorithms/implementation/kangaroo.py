a3e8f8da4fadc802838832be5081c1f9848bdebf
Jatin wadhwa <jatinw21@gmail.com>
2016-12-05 09:56:41 +1100

    shorthand conditionals

---
+++ algorithms/implementation/kangaroo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+x1, v1, x2, v2 = [int(x) for x in input().split(' ')]
+
+
+# cases:
+# x1 > x2 , v1 > v2 NO
+# x1 > x2 , v2 > v1 CAN MEET
+# x1 < x2 , v1 > v2 CAN MEET
+# x1 < x2 , v2 > v1 NO
+
+flag = 0
+
+if x1 < x2 and v1 > v2:
+    while x1 < x2:
+         x1 += v1
+         x2 += v2
+         if x2 == x1:
+             flag = 1
+             break
+elif x1 > x2 and v2 > v1:
+    while x2 < x1:
+        x1 += v1
+        x2 += v2
+        if x2 == x1:
+            flag = 1
+            break
+
+print("YES") if flag == 1 else print("NO")
