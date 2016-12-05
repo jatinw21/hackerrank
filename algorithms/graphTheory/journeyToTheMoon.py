18f5fb2676c7f70701639ee5faf1ca76585b4bed
Jatin wadhwa <jatinw21@gmail.com>
2016-12-05 15:04:43 +1100

    Working correctly (hopefully) but taking too long acc to hackerrank

--- algorithms/graphTheory/journeyToTheMoon.py
+++ algorithms/graphTheory/journeyToTheMoon.py
@@ -8,22 +8,34 @@ countryMembers = []
 countryMembers.append({int(x) for x in input().split(' ')})
 countryCounter += 1
 
+
 for i in range(1, numRel):
+    matchFound = 0
     currMem1, currMem2 = [int(x) for x in input().split(' ')]
-
+    stage = 0
     for j in range(len(countryMembers)):
-        # check if currMem1 or currMem2 is in set countryMembers[j]
-        if currMem1 in countryMembers[j] or currMem2 in countryMembers[j]:
-            countryMembers[j].update([currMem1, currMem2])
-            break
-    else:
-        # loop fell through without finding these in any existing country
-        countryMembers.append({currMem1, currMem2})
-        countryCounter += 1
 
-print(countryMembers)
+            # check if currMem1 or currMem2 is in set countryMembers[j]
+        if currMem1 in countryMembers[j] or currMem2 in countryMembers[j]:
+            matchFound = 1
+            if stage == 0:
+                countryMembers[j].update([currMem1, currMem2])
+                matchFoundCounter = j
+                stage = 1
+            else: # already found match, added this to that set, now wherever match is found
+            # add that set to the noted set
+                countryMembers[matchFoundCounter] = countryMembers[matchFoundCounter].union(countryMembers[j])
+                del countryMembers[j]
+                countryCounter -= 1
+        if matchFound == 0:
+            # loop fell through without finding these in any existing country
+            countryMembers.append({currMem1, currMem2})
+            countryCounter += 1
+    # print(countryMembers)
+
+# print(countryMembers)
 listOfNumSame = [len(countryMembers[i]) for i in range(countryCounter)]
-print(listOfNumSame)
+# print(listOfNumSame)
 
 numAstInSame = 0
 for i in listOfNumSame:
@@ -37,7 +49,7 @@ for i in range(astWithNoSameCountry):
 
 ans = 0
 # Do calculation
-print("After update.", listOfNumSame)
+# print("After update.", listOfNumSame)
 
 for i in range(len(listOfNumSame)):
     for j in range(i+1,len(listOfNumSame)):
