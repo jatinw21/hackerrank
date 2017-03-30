6a6c356408fc3412e8de0999c94d54d75deec521
Jatin Wadhwa <j.wadhwa@student.unsw.edu.au>
2017-03-31 02:49:34 +1100

    made code clearer

--- algorithms/implementation/betweenTwoSets.py
+++ algorithms/implementation/betweenTwoSets.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# the range of numbers is small, only upto 100, so this can be done
 numA, numB = [int(x) for x in input().strip().split(' ')]
 A = [int(x) for x in input().strip().split(' ')]
 B = [int(x) for x in input().strip().split(' ')]
 
+# the range of numbers is small, only upto 100, so this can be done
 # get all factors of B[0], these are possible x
 # for each element in B, check if all x are factors
 #   if yes, pass
@@ -15,43 +15,33 @@ B = [int(x) for x in input().strip().split(' ')]
 #   if yes, good
 #   if no, remove that x
 
-# now list is the ans
 possibleX = []
 k = B[0]
 
-# print(B)
+# factors of first element in B for possible X
 for i in range(1, k + 1): # checking for all elements upto half
     if k % i == 0:
         possibleX.append(i) # these are factors of k
-# print("possible x")
-# print(possibleX)
 
+# removing factors in not in other elements of B
 removeList = []
-
 for n in B[1:]:
-    # print("n: " + str(n))
     removeList = []
     for x in possibleX:
         if n % x != 0:
             removeList.append(x)
 
-    # print(n, removeList,possibleX)
     for r in removeList:
         possibleX.remove(r)
 
-    # print(possibleX)
-#
-#
-# print(possibleX)
-# print("=========================")
+# removing the x which do not have all elements of A as factors
 removeSet = set()
 for x in possibleX:
-    # print("x: " + str(x))
     for a in A:
         if x % a != 0:
             removeSet.add(x)
 
 for r in removeSet:
     possibleX.remove(r)
-#
+
 print(len(possibleX))
